b6ca3a7f37b12c8fccf0125ad5361dda1c73fc1c
Carlos <carlosrmd94@gmail.com>
2017-03-26 12:25:10 -0400

    songs and genres arrays created with pertinent information about the songs and their genres respectively

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,4 +1,5 @@
 from sklearn.neural_network import MLPClassifier
+from os import walk
 import sys
 import numpy as np
 sys.path.insert(0, './MSongsDB/PythonSrc')
@@ -15,7 +16,12 @@ def extractSongData(file_name, getters_to_apply):
     h5.close()
     return song
 
+
 if __name__ == '__main__':
+
+    songs = []
+    genres = []
+    all_songfiles_dict = {}
 
     list_of_getters = filter(lambda x: x[:4] == 'get_', getters.__dict__.keys())
     getters_to_remove = ['get_song_id', 'get_release', 'get_artist_hotttnesss', 'get_title', 'get_artist_longitude',
@@ -32,5 +38,24 @@ if __name__ == '__main__':
 
     # Aqui va el codigo para iterar
 
-    file_name = #nombre del archivo de la cancion sin la extencion
-    song = extractSongData(file_name,getters_to_apply) # retorna los datos de la cancion
+    all_songs_file = open('canciones_generos.cls','r')
+
+    for line in all_songs_file:
+        act_line = line.split('\t')
+        all_songfiles_dict[act_line[0]] = act_line[1][:-1]
+
+    all_songs_file.close()
+
+    f = []
+    for (dirpath, dirnames, filenames) in walk('canciones'):
+        f.extend(filenames)
+        break
+    for elem in f:
+        file_name = elem[:-3]                              # nombre del archivo de la cancion sin la extension
+        genre = all_songfiles_dict[file_name]              # Genero de la cancion
+        song = extractSongData(file_name,getters_to_apply) # retorna los datos de la cancion
+        songs.append(song)
+        genres.append(genre)
+
+    for i in range(len(songs)):
+        print("\nCancion: " + str(i+1) + ' ' + str(songs[i]) + "\nGenero: " + str(genres[i]) + '\n')
